ebca8d5d9876394fc0b8c5f43d860cac65a1bedc
jxf <jxf2008@outlook.com>
2021-03-16 09:15:41 +0800

    添加图片

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    /*QString qssFilePath = QCoreApplication::applicationDirPath() + "/" + "AppStyle.qss";
+    qssFilePath = "E:/";
+    qssFilePath += "AppStyle.qss";
+    QFile qssFile(qssFilePath);
+    qssFile.open(QIODevice::ReadOnly);
+    QTextStream textStream(&qssFile);
+    QString styleStr = textStream.readAll();
+    a.setStyleSheet(styleStr);*/
     ChessRobert w;
     w.show();
 
